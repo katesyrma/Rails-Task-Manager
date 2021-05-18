3d0c70e4c5f54dc3a2349d1113f37d0074bef5cf
Kate Syrmakesi <kate.syrmakesi@gmail.com>
2021-05-18 12:55:31 +0200

    4 - As a user, I can view the details of a task >> NO FORMATTING YET

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # READ - all
+  get 'tasks', to: 'tasks#index'
+
+  # READ - one
+  get 'tasks/:id', to: 'tasks#show', as: :task
 end
