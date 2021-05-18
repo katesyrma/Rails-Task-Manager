a36bb88fd7deb8e45690aa9a01c766aa7d1c3405
Kate Syrmakesi <kate.syrmakesi@gmail.com>
2021-05-18 16:51:35 +0200

    2 - Rails Task Manager >> Done, not styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # CREATE A TASK
+  # 1. display the task
+  get 'tasks/new', to: 'tasks#new'
+  # 2. send the new task form (post)
+  post 'tasks', to: 'tasks#create'
+
   # READ - all
   get 'tasks', to: 'tasks#index'
 
   # READ - one
   get 'tasks/:id', to: 'tasks#show', as: :task
+
+  # UPDATE a task
+  # 1. display the form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :task_edit
+  # 2. send form
+  patch 'tasks/:id', to: 'tasks#update', as: :task_update
+
+  # DELETE a task
+  delete 'tasks/:id', to: 'tasks#destroy', as: :task_destroy
 end
